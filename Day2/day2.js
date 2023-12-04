70f298bb5a2eae80b62acf6e145e0e091e73ac35
Alex Arutian <alexarutian@gmail.com>
2023-12-03 20:10:08 -0800

    day 2 part 2

--- Day2/day2.js
+++ Day2/day2.js
@@ -7,11 +7,14 @@ filePath = path.join(__dirname, "day2.txt");
 
 let maxByColor = {"blue": 14, "green": 13, "red": 12}
 let cumulativeTotal = 0
+let cumulativeTotalPart2 = 0
 
 fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
   if (!err) {
 
+    
     const lines = data.split("Game");
+
     for (let i = 0; i < lines.length; i++) {
       let line = lines[i];
       line = line.trim()
@@ -21,6 +24,8 @@ fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
       let draws = line.split(":")[1];
       let gameId = line.split(":")[0]
       gameId = Number(gameId)
+
+      let minGameCounts = {"red": 0, "green": 0, "blue": 0}
       if (draws != undefined) {
         draws = draws.split(";");
         for (let draw of draws) {
@@ -34,10 +39,16 @@ fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
                 let color = drawData[1]
                 let amount = Number(drawData[0])
 
+                // part 1
                 if (amount > maxByColor[color]) {
                     // console.log("amount" + amount +" color:" + color)
                     gameIsValid = false;
                 } 
+
+                // part 2
+                if (amount > minGameCounts[color]) {
+                    minGameCounts[color] = amount
+                }
             }
 
         }
@@ -47,8 +58,12 @@ fs.readFile(filePath, { encoding: "utf-8" }, function (err, data) {
         console.log(line)
         cumulativeTotal = cumulativeTotal + gameId
       }
+
+      let power = minGameCounts["red"] * minGameCounts["green"] * minGameCounts["blue"]
+      cumulativeTotalPart2 = cumulativeTotalPart2 + power
     }
     console.log(cumulativeTotal)
+    console.log(cumulativeTotalPart2)
 
   }
 });
